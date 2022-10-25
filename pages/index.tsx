5e64ede728d677340ff72464bfa82889c5d1135d
Austin Calvelage <austin.calvelage@icloud.com>
2022-10-25 00:37:04 -0400

    added footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import CompanyTraits from "./components/CompanyTraits";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import HowItWorks from "./components/HowItWorks";
@@ -7,13 +8,14 @@ import ProductSection from "./components/ProductSection";
 
 const Home: NextPage = () => {
   return (
-    <div className="space-y-4 px-3 pt-4 md:px-5 lg:px-10">
+    <div className="space-y-4 px-3 py-4 md:px-5 lg:px-10">
       <Header />
       <main className="space-y-15">
         <HeroSection page="home" />
         <ProductSection />
         <CompanyTraits />
         <HowItWorks />
+        <Footer />
       </main>
     </div>
   );
