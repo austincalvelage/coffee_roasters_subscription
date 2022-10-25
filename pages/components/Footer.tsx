c25ef1161a4f696f36281f7f51c7d238e5f149b0
Austin Calvelage <austin.calvelage@icloud.com>
2022-10-25 18:54:34 -0400

    Adjusted footer styles for responsiveness

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -31,11 +31,11 @@ function Footer({}: Props): JSX.Element {
 
   return (
     <footer className="bg-[#2C343E] py-6">
-      <Grid classes="space-y-4">
-        <div className="col-span-4 col-start-2 md:col-start-5">
+      <Grid classes="space-y-4 xl:space-y-0">
+        <div className="col-span-4 col-start-2 md:col-start-5 xl:col-span-2 xl:col-start-2">
           <Image src={logo} alt="logo" layout="responsive" />
         </div>
-        <nav className="col-span-full col-start-1 flex flex-col items-center space-y-3 font-barlow text-xs font-bold tracking-widest text-secondary">
+        <nav className="col-span-full col-start-1 flex flex-col items-center space-y-3 font-barlow text-xs font-bold tracking-widest text-secondary md:flex-row md:justify-center md:space-y-0 xl:col-span-4 xl:col-start-5 xl:justify-start">
           {links.map(({ link, name }, index) => (
             <div key={index}>
               <Link href={link}>
@@ -44,12 +44,12 @@ function Footer({}: Props): JSX.Element {
             </div>
           ))}
         </nav>
-        <div className="col-span-full col-start-1 flex justify-center gap-3">
+        <div className="col-span-full col-start-1 flex items-center justify-center gap-3 xl:col-span-1 xl:col-start-11">
           {socialLinks.map((socialLink, index) => (
             <div key={index}>
               <Link href="#">
                 <a
-                  className="bg-contain bg-no-repeat px-2 py-1"
+                  className="bg-contain bg-no-repeat px-2 py-1 xl:px-1 xl:py-0"
                   style={{ backgroundImage: `url(${socialLink.src})` }}
                 />
               </Link>
