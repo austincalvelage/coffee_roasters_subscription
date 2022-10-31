a06d5c6b7b91b15c593120b53f9d3f63b4d4614a
Austin Calvelage <austin.calvelage@icloud.com>
2022-10-31 18:28:30 -0400

    About section WIP

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from "next";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import OurCommitment from "./components/OurCommitment";
+import Quality from "./components/quality";
 
 const About: NextPage = () => {
   return (
     <div className="space-y-4 px-3 pt-4 md:px-5 lg:px-10">
       <Header />
-      <HeroSection page="about" />
+        <main className="space-y-15">
+          <HeroSection page="about" />
+          <OurCommitment />
+          <Quality />
+        </main>
     </div>
   );
 };
---
+++ pages/components/Quality.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Grid from "./Grid";
+
+function Quality(): JSX.Element {
+  return (
+    <section>
+      <Grid>
+        <div className="relative col-span-full col-start-1 bg-[#2C343E] flex flex-col items-center rounded-xs px-3 md:px-7">
+            <div className="relative -top-5 h-[150px] w-full rounded-xs overflow-hidden md:h-[320px]">
+              <Image
+                src="/assets/about/mobile/image-quality.jpg"
+                alt="test"
+                layout="fill"
+                className="hidden"
+              />
+            </div>
+            <div className="text-[#FEFCF7] text-center space-y-3">
+              <h3 className="font-bold text-3xl font-fraunce leading-none">Uncompromising quality</h3>
+              <p className="font-barlow">
+                Although we work with growers who pay close attention to all
+                stages of harvest and processing, we employ, on our end, a
+                rigorous quality control program to avoid over-roasting or baking
+                the coffee dry. Every bag of coffee is tagged with a roast date
+                and batch number. Our goal is to roast consistent, user-friendly
+                coffee, so that brewing is easy and enjoyable.
+              </p>
+            </div>
+          </div>
+      </Grid>
+    </section>
+  );
+}
+
+export default Quality;
